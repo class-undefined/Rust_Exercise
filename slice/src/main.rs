857956c4516e1f47f560e9ae17dfc2cf5e556978
野漫横江 <1399226280@qq.com>
2021-11-18 00:58:03 +0800

    feat: slice

--- slice/src/main.rs
+++ slice/src/main.rs
@@ -1,22 +1,34 @@
-fn main() {
-    /* 分割单词 */
-    let s = String::from("hello world");
-    fn get_word(_s:&String) -> Vec<String> {
+fn first_word(s: &String) -> &str {
+        let bytes = s.as_bytes();
+
+        for (i, &item) in bytes.iter().enumerate() {
+            if item == b' ' {
+                return &s[0..i];
+            }
+        }
+
+        &s[..]
+}
+fn get_words(_s: &String) -> Vec<String> {
         let mut word: String = String::new();
         let mut ans: Vec<String> = Vec::new();
         for (i, ch) in _s.as_str().chars().enumerate() {
             if ch != ' ' {
                 word.push(ch);
             }
-            if ch == ' ' || i == _s.len() - 1{
+            if ch == ' ' || i == _s.len() - 1 {
                 ans.push(word.clone());
                 word.clear();
             }
         }
         ans
-    }
-    let result:Vec<String> = get_word(&s);
+}
+fn main() {
+    /* 分割单词 */
+    let s = String::from("hello world");
+    let result: Vec<String> = get_words(&s);
     for str in result.iter() {
         println!("{}", str);
     }
+    println!("{}", first_word(&s));
 }
