6cb8f7cef9eea6b8ac190b717d92241c745c4a5c
野漫横江 <1399226280@qq.com>
2021-11-17 19:25:26 +0800

    feat: mut

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -24,6 +24,20 @@ fn test3() {
     println!("{}", _v[0]);
 }
 
+/* 与javascript的const不同，Rust若不指定mut，则无法更改容器内的数据，这里的数据修改权不仅限于栈空间，更像是变量的可写权限？ */
+fn test4() {
+    let mut _v:Vec<i32> = Vec::new();
+    _v.push(1);
+
+    /* change函数得到了传递过来的Vec实例的所有权 */
+    let change = |v:& mut Vec<i32>| -> () {
+        v.push(3);
+        return;
+    };
+    change(&mut _v);
+    println!("{}", _v[1]);
+}
+
 fn main() {
     test1();
     test2();
