34feb63ca6d78610b44975351e1e801131bda782
野漫横江 <1399226280@qq.com>
2021-11-19 23:17:55 +0800

    feat: Trait Bound

--- template/src/main.rs
+++ template/src/main.rs
@@ -3,17 +3,17 @@ struct Point<T, U> {
     x: T,
     y: U
 }
-impl <T, U> Point<T, U> {
-    fn mixup<V, W>(self: Point<T, U>, other:Point<V, W>) -> Point<T, W> {
+impl <T: Clone, U: Clone> Point<T, U> {
+    fn mixup<V: Clone, W: Clone>(self: &Point<T, U>, other:&Point<V, W>) -> Point<T, W> {
         Point {
-            x: self.x,
-            y: other.y
+            x: self.x.clone(),
+            y: other.y.clone()
         }
     }
 }
 fn main() {
     let p1 = Point {x: 5, y: 10.4};
     let p2 = Point {x: "hello", y: 'c'};
-    let p3 = p1.mixup(p2);
+    let p3 = p1.mixup(&p2);
     println!("{:#?}", p3);
 }
