494d311c268c8e0c3be5e433d72e21a467b6e8f7
野漫横江 <1399226280@qq.com>
2021-11-19 19:09:51 +0800

    feat: 增加?用法、链式调用发以及fs::read_to_string api

--- result_demo/src/main.rs
+++ result_demo/src/main.rs
@@ -1,4 +1,4 @@
-use std::io;
+use std::{fs, io};
 use std::io::Read;
 use std::fs::File;
 fn read_username_from_file() -> Result<String, io::Error> {
@@ -13,12 +13,24 @@ fn read_username_from_file() -> Result<String, io::Error> {
         Err(e) => Err(e)
     }
 }
+
 fn read_usename_from_file_simple() -> Result<String, io::Error> {
     let mut f = File::open("main.rs")?;
     let mut s = String::new();
     f.read_to_string(&mut s)?;
     Ok(s)
 }
+
+fn read_username_from_file_link() -> Result<String, io::Error> {
+    let mut s = String::new();
+    File::open("main.rs")?.read_to_string(&mut s)?;
+    Ok(s)
+}
+
+fn read_username_from_file_api() -> Result<String, io::Error> {
+    fs::read_to_string("main.rs")
+}
+
 fn main () {
 
 }
