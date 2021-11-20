b5216a5ece7bc6086efb76b92a386172a171c7ef
野漫横江 <1399226280@qq.com>
2021-11-21 00:54:32 +0800

    feat: 生命周期

--- life/src/main.rs
+++ life/src/main.rs
@@ -3,6 +3,22 @@ fn test<'n>() -> &'n i32 {
     a
 }
 
+fn getVec() -> Vec<i32> {
+    let r:Vec<i32> = Vec::new();
+    r
+}
+
+fn iterator() {
+    let v: Vec<i32> = vec![1,2,3,4,5,6];
+    let m:Vec<_> = v.iter().filter(|n| {
+        n > &&3
+    }).collect();
+    println!("{} {}", v.len(), m.len());
+}
+
 fn main() {
-    println!("{}", test());
+    let x: Vec<i32> = Vec::new();
+    let f = |a:Vec<i32>| &a.len() == &x.len();
+    iterator();
+    println!("{}", x.len());
 }
