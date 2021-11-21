924e1c2012b94ca0bd649cef3979b5f7d48b236f
野漫横江 <1399226280@qq.com>
2021-11-21 20:21:05 +0800

    style: 删除trait

--- box_ptr/src/LinkListUtils.rs
+++ box_ptr/src/LinkListUtils.rs
@@ -27,12 +27,13 @@ pub struct LinkList<T: Clone + Display> {
     len: u32
 }
 impl <T: Clone + Display>LinkList<T> {
-    pub fn new() -> LinkList<T> {
-        Self {
-            head: None,
-            len: 0
+    pub fn new() -> Self {
+            LinkList {
+                head: None,
+                len: 0
+            }
         }
-    }
+    
     pub fn push_back(&mut self, e: T) -> () {
         let node = LinkNode::new(e);
         if self.head.is_none() {
@@ -68,58 +69,6 @@ impl <T: Clone + Display>LinkList<T> {
     }
 
     pub fn size(&mut self) -> u32 {
-        self.len
-    }
-
-}
-pub trait LinkTrait<T> {
-
-    // fn insert(index: i32) -> ();
-
-    fn push_from_vec(&mut self, v: Vec<T>) -> ();
-
-    fn push_back(&mut self, e: T) -> ();
-
-    // fn pop_back() -> ();
-
-    fn show(&mut self) -> ();
-
-    fn size(&mut self) -> u32;
-}
-
-impl <T: Clone + Display> LinkTrait<T> for LinkList<T> {
-    fn push_back(&mut self, e: T) -> () {
-        let node = LinkNode::new(e);
-        if self.head.is_none() {
-            self.head = Some(Box::new(node));
-            self.len += 1;
-            return;
-        }
-        let mut cursor = self.head.as_mut().expect("self.head.as_mut() error");
-        /* 如果next不为none */
-        while cursor.next.is_some() {
-            let node_next = cursor.next.as_mut().expect("cursor.next.as_mut() error");
-            cursor = node_next;
-        }
-        cursor.next = Some(Box::new(node));
-        self.len += 1;
-    }
-
-    fn push_from_vec(&mut self, vec: Vec<T>) -> () {
-        for e in vec {
-            self.push_back(e);
-        }
-    }
-
-    fn show(&mut self) -> () {
-        let mut cursor = self.head.as_mut().expect("msg");
-        while cursor.next.is_some() {
-            print!("{} ", cursor.val);
-            cursor = cursor.next.as_mut().expect("msg");
-        }
-    }
-
-    fn size(&mut self) -> u32 {
         self.len
     }
 
