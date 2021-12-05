e988a0827812a31ff735cf19eab5051f8865078b
野漫横江 <1399226280@qq.com>
2021-12-06 00:21:33 +0800

    feat: 增加获取树的深度

--- tree/src/main.rs
+++ tree/src/main.rs
@@ -6,5 +6,6 @@ fn main() {
     let mut tree: Tree<i32> = Tree::new();
     let nodes = [Some(1), None, Some(2), Some(3), Some(4), None, None, Some(5), None, Some(6), None, None];
     tree.create(&nodes);
-    println!("{:?}", tree);
+    let depth = tree.get_depth();
+    println!("depth:{} tree: {:?}", depth, tree);
 }
--- tree/src/tree.rs
+++ tree/src/tree.rs
@@ -1,3 +1,5 @@
+use std::collections::VecDeque;
+
 pub type Node<T> = Option<Box<TreeNode<T>>>;
 
 #[derive(Debug)]
@@ -26,6 +28,8 @@ pub trait TreeMethod<T: Copy> {
     fn new() -> Self;
 
     fn create(&mut self, nodes: &[Option<T>]) -> ();
+
+    fn get_depth(&self) -> usize;
 }
 
 impl <T: Copy>TreeNodeMethod<T> for TreeNode<T> {
@@ -57,5 +61,30 @@ impl <T: Copy>TreeMethod<T> for Tree<T> {
             }
         }
         self.root = _build(nodes, &mut 0);
+        self.depth = self.get_depth();
+    }
+    
+    /* bfs层序遍历 */
+    fn get_depth(&self) -> usize {
+        let mut queue = VecDeque::new();
+        let mut depth = 0;
+        if self.root.is_some() {
+            queue.push_back(self.root.as_ref());
+        }
+        while queue.is_empty() == false {
+            let size = queue.len();
+            for _ in 0..size {
+                let node = queue.pop_front().unwrap();
+                let node_unwarp = node.unwrap();
+                if node_unwarp.left.is_some() {
+                    queue.push_back(node_unwarp.left.as_ref());
+                }
+                if node_unwarp.right.is_some() {
+                    queue.push_back(node_unwarp.right.as_ref());
+                }
+            }
+            depth += 1;
+        }
+        depth
     }
 }
