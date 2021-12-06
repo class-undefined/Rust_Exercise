c3d0b7f304b7d5e89d9d6147cf83d309a6afe42a
野漫横江 <1399226280@qq.com>
2021-12-06 20:35:00 +0800

    feat: rc_ref单链表的remove

--- link_rc_ref_单链表/src/link.rs
+++ link_rc_ref_单链表/src/link.rs
@@ -1,4 +1,4 @@
-use std::borrow::Borrow;
+
 use std::cell::RefCell;
 use std::fmt::Display;
 use std::rc::Rc;
@@ -69,21 +69,57 @@ impl <T: Display>Link<T> {
         let mut curr = Rc::clone(self.root.as_ref().unwrap());
         for _i in 0..(begin - 1) {
             let _node = Rc::clone(&curr);
-            curr = Rc::clone(&_node.borrow_mut().next.as_ref().unwrap());
+            curr = Rc::clone(&_node.as_ref().borrow_mut().next.as_ref().unwrap());
         }
-        let post = if let Some(post_node) = &curr.borrow_mut().next {
+        let post = if let Some(post_node) = &curr.as_ref().borrow_mut().next {
             Some(Rc::clone(post_node))
         } else {
             None
         };
         node.set_next(post);
-        curr.borrow_mut().set_next(node.to_option());
+        curr.as_ref().borrow_mut().set_next(node.to_option());
         self.size += 1;
     }
 
     pub fn remove(&mut self, index: usize) -> () {
-        if index > self.size {
+        if index >= self.size {
             panic!("删除的位置index: {} 不合法", index);
+        }
+        if self.root.is_none() {
+            panic!("容器内无元素删除!");
+        }
+        /* 处理当删除元素是头节点的情况 */
+        if index == 0 {
+            let node_option = self.root.take();
+            let node = node_option.unwrap();
+            let next_node_option = &node.as_ref().borrow().next;
+            if next_node_option.is_none() {
+                self.root = None;
+                self.size -= 1;
+                return ;
+            }
+            let next_node = next_node_option.as_ref().unwrap();
+            self.root = Some(Rc::clone(next_node));
+            self.size -= 1;
+            return ;
+            /* 处理删除元素非头节点的情况 */
+        } else {
+            let mut curr_option = Rc::clone(self.root.as_ref().unwrap());
+            for _i in 0..index - 1 {
+                let next = Rc::clone(curr_option.borrow().next.as_ref().unwrap());
+                curr_option = next;
+            }
+            let mut curr_node = curr_option.borrow_mut();
+            let next_node_option = {
+                if curr_node.next.is_none() {
+                    None
+                } else {
+                    let next = curr_node.next.as_ref().unwrap();
+                    Some(Rc::clone(next))
+                }
+            };
+            curr_node.next = next_node_option;
+            self.size -= 1;
         }
 
 
@@ -97,7 +133,7 @@ impl <T: Display>Link<T> {
             let _node = Rc::clone(&curr);
             print!("{} ", node.as_ref().borrow().val);
             if node.as_ref().borrow().next.is_some() {
-                curr = Rc::clone(&_node.borrow_mut().next.as_ref().unwrap());
+                curr = Rc::clone(&_node.as_ref().borrow_mut().next.as_ref().unwrap());
             }
         }
     }
